86c5415fa9de2d2a258f901df9b7a57d141e770f
Payne Zhou <inpert@yahoo.ca>
2015-08-16 09:09:32 -0400

    updated data models for period and taxes

--- app/models/fm01-targetFake.js
+++
@@ -1,3 +0,0 @@
-﻿module.exports = {
-    "baseIRR": 10, "incometax": 0, "surplusreserve": 0, "Statutorywelfarereserve": 0, "extractsurplusreserve": "", "Profitpayable": 0, "surplusreservescale": 0
-}
