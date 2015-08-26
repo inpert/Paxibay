f41e39890abe8fe4ae1bd19041db789b844974dc
Payne Zhou <inpert@yahoo.ca>
2015-08-26 18:49:21 -0400

    initial process.env.NODE_ENV

--- boot.js
+++ boot.js
@@ -1,4 +1,8 @@
+// Set the 'NODE_ENV' variable
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var app = require('./config/express')(null);
 
 app.listen(5660);
 console.log('App listening on port 5660');
+console.log(process.env.NODE_ENV);
